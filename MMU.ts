4b583c26151bd0d1d52dd2bda7ea244c0ebc1a57
Danny <77023480+TheDanDan@users.noreply.github.com>
2024-04-22 18:37:39 -0400

    update

--- MMU.ts
+++
@@ -1,7 +0,0 @@
-const MMU = {
-  rb: function(addr: number) { return addr },
-  rw: function(addr: number) { return addr },
-
-  wb: function(addr: number, val: number) { addr = val },
-  ww: function(addr: number, val: number) { addr = val }
-};
